38496dbba4ae63dcf90795629e0627938e26eda7
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-06-24 19:50:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyle } from './Global';
+
+function App() {
+  return (
+    <>
+      <div>Hello from The App Component</div>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
