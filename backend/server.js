a4cc660f7877bec3ccae1d4f76bb4ee291942dbb
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-07-12 23:04:23 +0100

    serve all products and single product through backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+let products = null;
+const readData = async () => {
+  await fs.readFile(`${__dirname}/data/products.json`, 'utf-8', (err, data) => {
+    products = JSON.parse(data);
+  });
+};
+readData();
 
-app.listen(5000, console.log('Server runing on port 5000')) 
+app.get('/', (req, res) => {
+  res.send('Home page');
+});
+
+app.get('/api/v1/products', (req, res) => {
+  res.send(products);
+});
+
+app.get('/api/v1/products/:id', (req, res) => {
+  const product = products.data.find((el) => el._id === req.params.id);
+  console.log('product', product);
+  if (!product) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Cannot find this product',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    product,
+  });
+});
+
+app.listen(5000, console.log('Server runing on port 5000'));
