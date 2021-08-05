2bb199dd35f0b4f8d5876d811bdb8c02f80b08f2
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-08-05 14:26:40 +0100

    added remove cart item in SingleProduct Screen

--- frontend/src/helper/decrementCartQty.js
+++ frontend/src/helper/decrementCartQty.js
@@ -1,6 +1,13 @@
-export const decrementQty = (cartItems, cartItemAdded) =>
-  cartItems.map((cartItem) =>
-    cartItem._id === cartItemAdded._id
-      ? { ...cartItem, quantity: cartItem.quantity === 0 ? 0 : cartItem.quantity - 1 }
-      : cartItem
+export const decrementQty = (cartItems, cartItemToRemove) => {
+  const existingItemCart = cartItems.find((cartItem) => cartItem._id === cartItemToRemove._id);
+  if (existingItemCart && existingItemCart.quantity === 1) {
+    return cartItems.filter((el) => el._id !== cartItemToRemove._id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem._id === cartItemToRemove._id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
   );
+};
+
+// cartItem._id === cartItemAdded._id
+//     ? { ...cartItem, quantity: cartItem.quantity === 0 ? 0 : cartItem.quantity - 1 }
+//     : cartItem
