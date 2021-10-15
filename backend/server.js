320c6d7fa9a4a78caf76084e63638e0dcea892a3
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-10-15 11:25:33 +0100

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const productsRoutes = require('./routes/productsRoutes');
@@ -22,9 +23,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({ origin: true }));
 
-app.get('/', (req, res) => {
-  res.send('Home page');
-});
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
@@ -35,6 +33,19 @@ app.use('/api/v1/orders', orderRoute);
 app.use('/create-checkout-session', checkoutRoute);
 app.use('/api/v1/upload', uploadRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(path.resolve(), '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(path.resolve(), '../frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 app.use('*', notFound);
 
 app.use(errorHandler);
