cb7504a3370b14a229299cdf35b6be19314e462c
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-10-16 08:58:00 +0100

    fix issue with path

--- backend/server.js
+++ backend/server.js
@@ -34,30 +34,15 @@ app.use('/create-checkout-session', checkoutRoute);
 app.use('/api/v1/upload', uploadRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(path.resolve(), '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(path.resolve(), 'frontend', 'build', 'index.html')
-    )
-  );
+  app.use(express.static(path.join(__dirname, '/../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
-//   console.log(path.resolve('./frontend', 'build', 'index.html'));
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve('./frontend', 'build', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
 
 app.use(notFound);
 
